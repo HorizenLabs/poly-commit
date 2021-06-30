d1f9f29d221fb39937b21a0abb4861bc6615c752
Phoinic <max@phoinic.com.ua>
2021-06-30 14:15:32 +0300

    MSM args trim

--- src/ipa_pc/mod.rs
+++ src/ipa_pc/mod.rs
@@ -4,7 +4,7 @@ use crate::{LabeledCommitment, LabeledPolynomial, LabeledRandomness};
 use crate::{PCRandomness, PCUniversalParams, Polynomial, PolynomialCommitment};
 use algebra::msm::VariableBaseMSM;
 use algebra::{SemanticallyValid, ToBytes, to_bytes, Field, PrimeField, UniformRand, Group, AffineCurve, ProjectiveCurve};
-use std::{format, vec};
+use std::{format, vec, cmp};
 use std::marker::PhantomData;
 use rand_core::RngCore;
 
@@ -53,7 +53,10 @@ impl<G: AffineCurve, D: Digest> InnerProductArgPC<G, D> {
         let scalars_bigint = scalars.par_iter()
             .map(|s| s.into_repr())
             .collect::<Vec<_>>();
-        let mut comm = VariableBaseMSM::multi_scalar_mul(&comm_key, &scalars_bigint);
+        let mut comm = VariableBaseMSM::multi_scalar_mul(
+            &comm_key[0..cmp::min(scalars_bigint.len(), comm_key.len())],
+            &scalars_bigint[0..cmp::min(scalars_bigint.len(), comm_key.len())]
+        );
         if randomizer.is_some() {
             assert!(hiding_generator.is_some());
             comm += &hiding_generator.unwrap().mul(randomizer.unwrap());
